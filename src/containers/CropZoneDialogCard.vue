35716a52b2ffbca933d2155fe264a93847bfa905
NathanAwpi <101899832+NathanAwpi@users.noreply.github.com>
2023-05-24 12:46:48 -0400

    Better manual cropping of images
    - Improved the manual cropping of images
    - When no autocropping data is provided, now defaults to a box in the center of the image

--- src/containers/CropZoneDialogCard.vue
+++ src/containers/CropZoneDialogCard.vue
@@ -43,37 +43,15 @@
       </v-slide-group>
     </div>
     <div class="d-flex justify-center">
-      <v-btn color="info"
-        @click="manualCrop"
-        variant="outlined"
-        >Manual Crop
-        <v-tooltip
-          activator="parent"
-          location="bottom"
-        >Middle click and drag to create a box.</v-tooltip>
-      </v-btn>
-      <div class="pr-2"></div>
       <v-btn
         color="info"
-        @click="clearCrop"
+        @click="resetCrop"
         variant="outlined"
-        
-      >Clear
+      >Reset
       <v-tooltip
           activator="parent"
           location="bottom"
-        >Clear the manual crop you made.</v-tooltip>
-      </v-btn>
-      <div class="pr-2"></div>
-      <v-btn
-        color="info"
-        @click="resetToAutomaticCrop"
-        variant="outlined"
-      >Automatic Crop
-      <v-tooltip
-          activator="parent"
-          location="bottom"
-        >Load the automatic crop.</v-tooltip>
+        >Reset the crop to the default crop.</v-tooltip>
       </v-btn>
     </div>
     <div class="d-flex justify-center">
@@ -97,9 +75,6 @@
         class="cropper"
         ref="cropper"
         @change="checkSaveCropperResults"
-        @mousedown.middle="beginDrag"
-        @mouseup.middle="endDrag"
-        @mousemove="mouseMove"
         :src="generateURL(selectedImage)"
         id="cropper"
         :resizeImage="{ wheel: false }"
@@ -147,11 +122,8 @@ export default {
       switchModel: true,
       selectedIndex: 0, // The index of the selected image
       cropperData: null,
-      manualCropMode: false, // If the user is manually cropping
       cropperModel: null,
-      origDragX: 0, // When manually cropping, the left x-coord of the box
-      origDragY: 0, // When manually cropping, the top y-coord of the box
-      manualCropping: false, // If the user is currently dragging the box
+      autoJustReset: true,
     };
   },
   props: {
@@ -233,115 +205,27 @@ export default {
           });
         }
       }
-      console.log("uploading files");
+      console.log("Uploading files");
       this.$props.uploadFiles();
+    },
+    resetCrop() {
+      console.log("Resetting crop.");
+      this.saveCropperResults([], this.selectedIndex);
+      this.resetToAutomaticCrop();
     },
     /**
      * Checks if the cropper results should be saved; if they should, saves them.
      * The cropper results will be saved if the default box has been modified and the user is in the "manual cropping" mode
      */
     checkSaveCropperResults() {
-      // Check if the box has not been modified
-      if(this.$refs.cropper.coordinates.left === 0
-      && this.$refs.cropper.coordinates.top === 0
-      && this.$refs.cropper.coordinates.width === 1
-      && this.$refs.cropper.coordinates.height === 1) return;
+      // Check for changes
+      if(!this.autoJustReset) {
 
-      // Make sure the user is cropping manually
-      if(!this.manualCropMode) return;
+        this.saveCropperResults(this.$refs.cropper.coordinates, this.selectedIndex);
+        console.log("Saving cropper results: ", this.$props.cropperResults[this.selectedIndex]);
 
-      this.saveCropperResults(this.$refs.cropper.coordinates, this.selectedIndex);
-    },
-    /**
-     * Initializes everything needed for the click-and-drag functionality of the cropper box
-     * @param {*} event The event. Passed by v-on.
-     */
-    beginDrag(event) {
-      if(!this.manualCropMode) return;
-
-      this.manualCropping = true;
-
-      let cropBox = document.getElementById('cropper').getBoundingClientRect();
-
-      this.origDragX = (event.clientX - cropBox.left) * this.$refs.cropper.imageSize.width / cropBox.width;
-      this.origDragY = (event.clientY - cropBox.top) * this.$refs.cropper.imageSize.height / cropBox.height;
-
-      this.$refs.cropper.setCoordinates({
-        left: this.origDragX,
-        top: this.origDragY,
-        width: 1,
-        height: 1
-      });
-    },
-    /**
-     * Updates the cropper box when  the mouse is moved and we are in the click-and-drag state
-     * @param {*} event The event. Passed by v-on.
-     */
-    mouseMove(event) {
-      if(this.manualCropping) {
-
-        let cropBox = document.getElementById('cropper').getBoundingClientRect();
-
-        this.$refs.cropper.setCoordinates({
-          left: this.origDragX,
-          top: this.origDragY,
-          width: (event.clientX - cropBox.left) * (this.$refs.cropper.imageSize.width / cropBox.width) - this.origDragX,
-          height: (event.clientY - cropBox.top) * (this.$refs.cropper.imageSize.height / cropBox.height) - this.origDragY,
-        });
       }
-      },
-      /**
-       * Finishes the click-and-drag cropper box. Sets the final coordinates of the box and saves them.
-       * @param {*} event The event. Passed by v-on.
-       */
-    endDrag(event) {
-      if(!this.manualCropMode) return;
-
-      let cropBox = document.getElementById('cropper').getBoundingClientRect();
-
-      this.$refs.cropper.setCoordinates({
-        left: this.origDragX,
-        top: this.origDragY,
-        width: (event.clientX - cropBox.left) * (this.$refs.cropper.imageSize.width / 586) - this.origDragX,
-        height: (event.clientY - cropBox.top) * (this.$refs.cropper.imageSize.width / 586) - this.origDragY,
-      });
-
-      this.saveCropperResults(this.$refs.cropper.coordinates, this.selectedIndex);
-
-      this.manualCropping = false;
-
-    },
-    /**
-     * Ititializes the manual crop mode when  the "manual crop" button is pressed. This means setting to cropper box coordinates to the saved coordinates, if there are any.
-     */
-    manualCrop() {
-      if(this.manualCropMode) return;
-      console.log("Entering manual crop mode.");
-      this.manualCropMode = true;
-      
-      // There are saved cropper results
-      if (this.$props.cropperResults[this.selectedIndex] !== undefined) {
-
-        this.$refs.cropper.setCoordinates({
-          left: this.$props.cropperResults[this.selectedIndex].left,
-          top: this.$props.cropperResults[this.selectedIndex].top,
-          width: this.$props.cropperResults[this.selectedIndex].width,
-          height: this.$props.cropperResults[this.selectedIndex].height,
-        });
-      }
-      // There are no saved cropper results
-      else {
-        this.$refs.cropper.setCoordinates({left: 0, top: 0, width: 1, height: 1});
-      }
-    },
-    /**
-     * Clears the cropper box and saves the results
-     */
-    clearCrop() {
-      if(this.manualCropMode) {
-        this.$refs.cropper.setCoordinates({left: 0, top: 0, width: 1, height: 1});
-      }
-      this.saveCropperResults(this.$refs.cropper.coordinates, this.selectedIndex);
+      this.autoJustReset = false;
     },
     /**
      * Changes the crop mode to automatic crop:
@@ -350,21 +234,47 @@ export default {
      */
     resetToAutomaticCrop() {
 
-      this.$refs.cropper.setCoordinates({
-          left: this.$props.croppedApiResults[this.selectedIndex].xmin,
-          top: this.$props.croppedApiResults[this.selectedIndex].ymin,
-          width:
-            this.$props.croppedApiResults[this.selectedIndex].xmax -
-            this.$props.croppedApiResults[this.selectedIndex].xmin,
-          height:
-            this.$props.croppedApiResults[this.selectedIndex].ymax -
-            this.$props.croppedApiResults[this.selectedIndex].ymin,
+      this.autoJustReset = true;
+
+      // Check if there are saved cropper results
+      if(this.$props.cropperResults[this.selectedIndex] !== undefined && this.$props.cropperResults[this.selectedIndex].length !== 0) {
+        console.log("Found saved cropper results: ", this.$props.cropperResults[this.selectedIndex]);
+        // There are saved results, use those
+        this.$refs.cropper.setCoordinates({
+          left: this.$props.cropperResults[this.selectedIndex].left,
+          top: this.$props.cropperResults[this.selectedIndex].top,
+          width: this.$props.cropperResults[this.selectedIndex].width,
+          height: this.$props.cropperResults[this.selectedIndex].height,
         });
+      }
+      else {
+        // There are no saved results, default to the automatic crop
+        console.log("No saved cropper results.");
 
-        console.log("Resetting to automatic cropping.", this.$props.croppedApiResults[this.selectedIndex]);
-
-        this.manualCropMode = false;
-
+        // If the confidence is 0, there was no result => use a default result
+        if(this.$props.croppedApiResults[this.selectedIndex].confidence === 0) {
+          console.log("Confidence was 0. Changing to autogenerated crop.");
+          this.$refs.cropper.setCoordinates(({ coordinates, imageSize }) => ({
+            left: (imageSize.width - imageSize.width * .45) / 2,
+            top: (imageSize.height - imageSize.height * .95) / 2,
+            width: imageSize.width * 0.45,
+            height: imageSize.height * 0.95,
+          }));
+        }
+        else {
+          // Confidence was not 0, there is a result, use that
+          this.$refs.cropper.setCoordinates({
+            left: this.$props.croppedApiResults[this.selectedIndex].xmin,
+            top: this.$props.croppedApiResults[this.selectedIndex].ymin,
+            width:
+              this.$props.croppedApiResults[this.selectedIndex].xmax -
+              this.$props.croppedApiResults[this.selectedIndex].xmin,
+            height:
+              this.$props.croppedApiResults[this.selectedIndex].ymax -
+              this.$props.croppedApiResults[this.selectedIndex].ymin,
+        });
+        }
+      }
     },
     nextFile() {
       let index = 0;
@@ -453,16 +363,7 @@ export default {
         this.isBlurred = true;
       }
 
-      this.$refs.cropper.setCoordinates({
-        left: this.$props.croppedApiResults[0].xmin,
-        top: this.$props.croppedApiResults[0].ymin,
-        width:
-          this.$props.croppedApiResults[0].xmax -
-          this.$props.croppedApiResults[0].xmin,
-        height:
-          this.$props.croppedApiResults[0].ymax -
-          this.$props.croppedApiResults[0].ymin,
-      });
+      this.resetToAutomaticCrop();
     }
   },
 };
