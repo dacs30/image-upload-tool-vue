9cf2e00b35ef25b630593c84fb67f62192579f51
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-06-26 18:17:39 -0400

    updated implementation of bounding box

--- src/components/CapturePhotoDialogComponent.vue
+++ src/components/CapturePhotoDialogComponent.vue
@@ -17,7 +17,7 @@
             </div>
             <div id="videoContainer">
                 <video id="videoCam" ref="videoCam">Video stream not available</video>
-                <div v-if="isCameraOpen" :style="boxStyle"></div>
+                <canvas v-if="isCameraOpen" id="boundingBoxCanvas"></canvas>
             </div>
             <div>
                 <br>
@@ -35,13 +35,16 @@
 </template>
 
 <style scoped>
-#videoCam {
-    /* width: 1080px;
-    height: 720px; */
-    /* width: 99vw; */
-    height: 66vh;
-    margin: 0 auto;
-    border: 1px solid black;
+#videoCam{
+    position: relative;
+    width: 40vw;
+    height: auto;
+}
+#boundingBoxCanvas {
+    position: absolute;
+    width: 20vw;
+    height: 50vh;
+    border: 5px solid red;
 }
 .openCamBtn {
     margin: 1% 10%;
@@ -66,21 +69,18 @@
     text-align: center;
 }
 #videoContainer {
-  position: relative;
-  border: 1px solid black;
-}
-#videoContainer > div {
-  position: absolute;
+    position: relative;
+    /* width: 100%; */
+    /* height: auto; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid black;
 }
 </style>
 
 
 <script>
-// import * as cocoSSD from '@tensorflow-models/coco-ssd';
-import * as tf from '@tensorflow/tfjs';
-import '@tensorflow/tfjs-backend-cpu';
-import '@tensorflow/tfjs-backend-webgl';
-import { loadGraphModel } from '@tensorflow/tfjs';
 
 export default {
     name: "CapturePhotoDialogComponent",
@@ -139,7 +139,6 @@ export default {
                     this.getWebCameraResolution();
                     this.isCameraOpen = true;
                     console.log("isCameraOpen: ", this.isCameraOpen);
-                    this.detectObjects();
                 };
             }).catch((e) => {
                 console.log("An error occured with getting webcam feed");
@@ -176,16 +175,16 @@ export default {
             //then close the dialog
             this.$emit("close-capture-photo");
         },
-        //crop the image to the size of the boxProps
-        cropImage() {
-            let canvas = this.$refs.intermediateCanvas;
-            let context = canvas.getContext('2d');
-            let img = new Image();
-            img.src = this.link;
-            context.drawImage(img, this.boxProps.left, this.boxProps.top, this.boxProps.width, this.boxProps.height, 0, 0, this.boxProps.width, this.boxProps.height);
-            let dataURL = canvas.toDataURL('image/png');
-            this.link = dataURL;
-        },
+        // //crop the image to the size of the boxProps
+        // cropImage() {
+        //     let canvas = this.$refs.intermediateCanvas;
+        //     let context = canvas.getContext('2d');
+        //     let img = new Image();
+        //     img.src = this.link;
+        //     context.drawImage(img, this.boxProps.left, this.boxProps.top, this.boxProps.width, this.boxProps.height, 0, 0, this.boxProps.width, this.boxProps.height);
+        //     let dataURL = canvas.toDataURL('image/png');
+        //     this.link = dataURL;
+        // },
         //function to capture a photo from the webcam feed
         // capturePhoto() {
         //     let video = this.$refs.videoCam;
@@ -211,7 +210,7 @@ export default {
             context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
             let videoRect = video.getBoundingClientRect();
-            let boxRect = document.querySelector('#videoContainer > div').getBoundingClientRect();
+            let boxRect = document.querySelector('#boundingBoxCanvas').getBoundingClientRect();
 
             let scaleX = video.videoWidth / videoRect.width;
             let scaleY = video.videoHeight / videoRect.height;
@@ -238,57 +237,25 @@ export default {
             //handleCancelBtn closes the dialog here not cancel
             this.handleCancelBtn();
         },
-        async detectObjects() {
-            const MODEL_URL = 'src/TFJSModel/model.json'; 
-            const model = await loadGraphModel(MODEL_URL);
-            const img = tf.browser.fromPixels(this.$refs.videoCam);
-            const resized = tf.image.resizeBilinear(img, [640, 480]);
-            const casted = resized.cast('int32');
-            const expanded = casted.expandDims(0);
-            const obj = await model.executeAsync(expanded);
+        // async detectObjects() {
+        //     const MODEL_URL = 'src/TFJSModel/model.json'; 
+        //     const model = await loadGraphModel(MODEL_URL);
+        //     const img = tf.browser.fromPixels(this.$refs.videoCam);
+        //     const resized = tf.image.resizeBilinear(img, [640, 480]);
+        //     const casted = resized.cast('int32');
+        //     const expanded = casted.expandDims(0);
+        //     const obj = await model.executeAsync(expanded);
 
-            console.log("obj: ", obj);
-            const boxes = await obj[4].array();
-            const classes = await obj[5].array();
-            const scores = await obj[6].array();
-            console.log("boxes: ", boxes);
-            console.log("classes: ", classes);
-            console.log("scores: ", scores);
-            // const predictions = await model.predict(this.$refs.videoCam);
-            // console.log("predictions: ", predictions);
-        }
-    },
-    computed: {
-        boxStyle() {
-            let video = this.$refs.videoCam;
-            let videoWidth = 66 * video.videoWidth / 100; // adjusting for 66 vh
-            let videoHeight = 66 * video.videoHeight / 100;
-
-            //get the position of the video element
-            let videoPos = video.getBoundingClientRect();
-            let videoXPos = videoPos.left;
-            let videoYPos = videoPos.top;
-
-            //calculate the box size
-            let boxWidth = videoWidth * 0.4;
-            let boxHeight = videoHeight * 0.8;
-            let boxXPos = videoXPos + (videoWidth * 0.3);
-            let boxYPos = videoYPos - (videoHeight * 0.2);
-
-            this.boxProps.width = boxWidth;
-            this.boxProps.height = boxHeight;
-            this.boxProps.top = boxYPos;
-            this.boxProps.left = boxXPos;
-
-            return {
-                position: 'absolute',
-                left: `${boxXPos}px`,
-                top: `${boxYPos}px`,
-                width: `${boxWidth}px`,
-                height: `${boxHeight}px`,
-                border: '10px solid red',
-            };
-        },
+        //     console.log("obj: ", obj);
+        //     const boxes = await obj[4].array();
+        //     const classes = await obj[5].array();
+        //     const scores = await obj[6].array();
+        //     console.log("boxes: ", boxes);
+        //     console.log("classes: ", classes);
+        //     console.log("scores: ", scores);
+        //     // const predictions = await model.predict(this.$refs.videoCam);
+        //     // console.log("predictions: ", predictions);
+        // }
     },
 }
 
